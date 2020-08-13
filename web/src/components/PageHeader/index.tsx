85f0d51a56b559233d2248350d0d632b0019c8d4
glaucoserrano <51676475+glaucoserrano@users.noreply.github.com>
2020-08-13 09:23:40 -0300

    Mobile
    Final da Next Level Week, não finalizado mas conhecimentos absolvidos.

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -6,6 +6,7 @@ import backIcon from '../../assets/images/icons/back.svg';
 import './styles.css'
 interface PageHeaderProps{
    title: string;
+   description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -21,6 +22,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
          <strong>
              {props.title}
           </strong>
+          {props.description && <p>{props.description}</p>}
           {props.children}
       </div>
    </header>
--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,29 +1,48 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher{
+   id:number,
+   avatar:string,
+   bio:string,
+   cost:number,
+   name:string,
+   subject:string,
+   whatsapp: string,
+}
+interface TeacherItemPropos{
+   teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemPropos> =({teacher}) =>{
+   function createNewConnection(){
+      api.post('connections', {
+         user_id: teacher.id,
+      })
+   }
    return(
       <article className="teacher-item">
       <header>
-         <img src="https://media.istockphoto.com/vectors/teacher-avatar-vector-icon-vector-id1135046915" alt="sem foto"/>
+         <img src={teacher.avatar} alt={teacher.name}/>
          <div>
-            <strong>Nome do professor</strong>
-            <span>Matéria</span>
+            <strong>{teacher.name}</strong>
+            <span>{teacher.subject}</span>
          </div>
       </header>
       <p>
-         Biografia cadastrada
+         {teacher.bio}
       </p>
       <footer>
          <p>
             Preço/hora
-            <strong>R$ 9999,99</strong>
+            <strong>R$ {teacher.cost}</strong>
          </p>
-         <button type="button">
+         <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
             <img src={whatsappIcon} alt="Whatsapp"/>
             Entrar em contato
-         </button>
+         </a>
       </footer>
       </article>
    );
